781a1a5ebcade36a99ebfecb20b4693178bd51c1
Abhi Kathiriya <kathiriaabhi143@gmail.com>
2025-10-01 11:09:54 +0530

    Updated IpAddress

--- backend/Program.cs
+++ backend/Program.cs
@@ -76,7 +76,7 @@ namespace minutechart
             {
                 options.AddPolicy("AllowReactApp", policy =>
                 {
-                    policy.WithOrigins("http://localhost:3000", "http://192.168.1.102:3000")
+                    policy.WithOrigins("http://192.168.1.102:3000", "https://minutechart.vercel.app")
                           .AllowAnyHeader()
                           .AllowAnyMethod()
                           .AllowCredentials();
--- backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
+++ backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08646727d124e8dcb143cb671367d18b96e739ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0044c7052e530aea13fbda30997df866a2b3de6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
