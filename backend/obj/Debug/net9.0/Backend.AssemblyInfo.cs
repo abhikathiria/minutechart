99d0b69d57b1dc7f757cc905bd9cce279dda0705
Abhi Kathiriya <kathiriaabhi143@gmail.com>
2025-10-11 12:01:13 +0530

    Updated Database

--- backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
+++ backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f2b0145ec095c3bb164edb1c4a1c220e716fa37")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+789c01e7064c844f84bce6171b9fc39f953b8325")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
