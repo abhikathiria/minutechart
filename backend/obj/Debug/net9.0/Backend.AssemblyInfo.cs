f3a7c6f3f90cb2bc9f08877f4f4283a5adbbc35d
Abhi Kathiriya <kathiriaabhi143@gmail.com>
2025-10-16 12:27:52 +0530

    chanegd ef core version

--- backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
+++ backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ee377e5074c9a225c8754b1498d78a54f36125c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+225deccec68b965d94eb1dfcb9813293f2e467c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
