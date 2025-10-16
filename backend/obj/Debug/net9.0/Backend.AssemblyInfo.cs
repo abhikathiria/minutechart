1ee377e5074c9a225c8754b1498d78a54f36125c
Abhi Kathiriya <kathiriaabhi143@gmail.com>
2025-10-16 12:13:28 +0530

    updated backendcsporj

--- backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
+++ backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f0ff39d8ffcf825a3e8e7fc6b9e75f5d38124dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd7cfddb97ef4bfb6cf5b680716d75d2ae0c73f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
