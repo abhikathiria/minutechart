13bd8392724674ecfc7e52e1163b2b6c552103d7
Abhi Kathiriya <kathiriaabhi143@gmail.com>
2025-10-21 15:34:17 +0530

    updated appsettings

--- backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
+++ backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d157a4439d0aae7538e36954b3fb061390681335")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e80c9c249819f02cb43fc41eb282e5c57b65921")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
