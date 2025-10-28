f061896a1fc2454b61f1c4951e34040bcfbc4c4b
Abhi Kathiriya <kathiriaabhi143@gmail.com>
2025-10-28 17:08:15 +0530

    removed itemgroup

--- backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
+++ backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9802db716d913103813979195aa31d62278ef9b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b6a0c33e2b5234a16ea4c272d128ff041723343")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
